a492bab39d7c3c4c776642b2bdd72177ec9e9798
Rivan2505 <rivanpatel255@gmail.com>
2025-08-11 12:08:14 +1000

    vercel.json test7

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://melbourne-smart-parking.vercel.app'],
   credentials: true
 }));
 app.use(morgan('combined'));
