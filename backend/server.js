f7d49eeceff9e4c46ea3343d59b0685075baff2b
Rivan2505 <rivanpatel255@gmail.com>
2025-08-11 11:41:08 +1000

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
 
-const app = express();
+const app = express(); 
 const PORT = process.env.PORT || 5000;
 
 // Middleware
